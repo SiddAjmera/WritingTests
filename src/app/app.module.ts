723454df86187b4bd90147f6112870b50b1245b1
Siddharth Ajmera <Siddharth.Ajmera@T-Mobile.com>
2018-02-25 20:33:16 +0530

    Updated the App Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
+import { routes } from './app.routes';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     FormsModule,
     HttpModule
